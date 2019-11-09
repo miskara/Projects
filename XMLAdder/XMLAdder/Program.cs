9d706aaf44fa0700f10dd2a71cb30ab880f736b6
Miska Rantanen <rantanen.miska@gmail.com>
2019-11-09 23:23:01 +0200

    upload rekteht

---
+++ XMLAdder/XMLAdder/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+
+namespace XMLAdder
+{
+    class Program
+    {
+        public static XElement root = null;
+        public static XElement str = null;
+        public static string name = "";
+        static void Main()
+        {
+            string spath = "xmlharjoitusHiQ.xml";
+            name = "neljästesti";
+            XDocument doc = XDocument.Load(spath);
+            str = XElement.Parse(doc.ToString());
+            List<XElement> result = new List<XElement>();
+
+            result = str.Elements("item").
+            Where(x => x.Element("name").Value.Equals("foo46")).ToList();
+
+            if (result.Count>0)
+            {
+                root = result[0];
+                AddElement(str);
+            }
+
+            FindElement(result, str.Elements());
+
+            str.Save(spath);
+        }
+
+        private static void FindElement(List<XElement> result, IEnumerable<XElement> elements)
+        {
+            foreach (XElement el in elements)
+            {
+                result = el.Elements("item").
+                Where(x => x.Element("name").Value.Equals("foo46")).ToList();
+
+                if (result.Count > 0)
+                {
+                    root = result[0];
+                    AddElement(root.Parent);
+                    break;
+                }
+                else
+                {
+                    FindElement(result, el.Elements());
+                }
+            }
+        }
+
+        public static void AddElement(XElement el) 
+        {
+            if (root.HasElements && root.Descendants("children").Any())
+            {
+                XElement newEle = new XElement("item");
+                newEle.Add(new XElement("name", name));
+                el.Element(root.Name).Element("children").Add(newEle);
+            }
+            else
+            {
+                XElement newEle = new XElement("children");
+                newEle.Add(new XElement("item", new XElement("name", name)));
+                el.Element(root.Name).Add(newEle);
+            }
+        }
+    }
+}
---
+++ XMLPars/XMLPars/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+
+
+namespace XMLPars
+{
+
+    class Program
+    {
+        public static int intCount = 0;
+        public static string strParentName = "";
+        public static string strDelimeter = ";";
+        public static List<String> lstWriteToFile = new List<String>();
+        static void Main(string[] args)
+        {
+
+
+            XmlDocument doc = new XmlDocument();
+            doc.Load("xmlharjoitusHiQ.xml");
+            
+
+            TraverseNodes(doc.ChildNodes,"");
+
+            while (doc.NextSibling!=null)
+            {
+                TraverseNodes(doc.NextSibling.ChildNodes,"");
+            }
+
+            WriteRows(lstWriteToFile, "testi.txt");
+            
+        }
+
+        private static void TraverseNodes(XmlNodeList nodes, String strOldestParentName)
+        {
+            foreach (XmlNode node in nodes)
+            {
+                
+                if (node.Name=="name")
+                {                    
+                    lstWriteToFile.Add(intCount.ToString() + strDelimeter +
+                    node.InnerText + strDelimeter +
+                    strOldestParentName + strDelimeter +
+                    strParentName);
+
+                    if (strOldestParentName == "")
+                    {
+                        strOldestParentName = node.InnerText;
+                    }
+
+                    if (node.HasChildNodes && node.FirstChild.Name!="#text")
+                    {
+                        strParentName= node.InnerText;
+                    }
+                    intCount++;
+
+                }
+                TraverseNodes(node.ChildNodes,strOldestParentName);
+            }
+        }
+
+        public static void WriteRows(List<string> result, string path)
+        {
+            using (StreamWriter writer = new StreamWriter(path))
+            {
+                foreach (string element in result)
+                {
+                    writer.WriteLine(element);
+                }
+            }
+        }
+
+    }
+}
